e6152130afabd24e7ded396b95c83a5e7127240e
Emmanuel <emmkga@gmail.com>
2017-08-21 09:05:26 +0200

    Dashboard changes-Mannu

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,28 +1,101 @@
-<div id="page-wrapper">
-    <div class="row">
-        <div class="col-lg-9">
-            <div class="row">
-                <div class="col-lg-3">
-                    Rejected claims
-                </div>
-                <div class="col-lg-3">
-                    Accepted claims
-                </div>
-                <div class="col-lg-3">
-                    Claims
-                </div>
-                <div class="col-lg-3">
-                    Claims
+<div class="row">
+    <div class="col-lg-9">
+        <div class="row tile_count" id="bgcolor">
+            <div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count">
+                <div class="data-dashboard" id="data-dashboard1">
+                    <span class="count_top"><i class="fa fa-user"></i> Accepted claims</span>
+                    <div class="count">2500</div>
+                    <span class="count_bottom"><i class="green">4% </i> From last Week</span>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    Claims graph overview
+            <div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count">
+                <div class="data-dashboard" id="data-dashboard2">
+                    <span class="count_top"><i class="fa fa-clock-o"></i> Rejected claims</span>
+                    <div class="count">123.50</div>
+                    <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count">
+                <div class="data-dashboard" id="data-dashboard3">
+                    <span class="count_top"><i class="fa fa-user"></i> Appointments</span>
+                    <div class="count green">2,500</div>
+                    <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count">
+                <div class="data-dashboard" id="data-dashboard4">
+                    <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
+                    <div class="count green">2,500</div>
+                    <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
                 </div>
             </div>
         </div>
-        <div class="col-lg-3">
-            User profile
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="x_title">
+                    <h2>Claims overview</h2>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="">
+                    <div class="x_content1">
+                    <div id="graph_bar_group" style="width:100%; height:280px;"></div>
+                  </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="x_title">
+                    <h2>Top Campaign Performance</h2>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-6">
+                        <div>
+                            <p>Facebook Campaign</p>
+                            <div class="">
+                                <div class="progress progress_sm">
+                                    <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="80"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <p>Twitter Campaign</p>
+                            <div class="">
+                                <div class="progress progress_sm">
+                                    <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="60"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-sm-12 col-xs-6">
+                        <div>
+                            <p>Conventional Media</p>
+                            <div class="">
+                                <div class="progress progress_sm">
+                                    <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="40"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <p>Bill boards</p>
+                            <div class="">
+                                <div class="progress progress_sm" style="width: 50%">
+                                    <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="50"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div> 
+    </div>
+    <div class="col-lg-3">
+        <div class="row">
+            <div class="col-lg-3">
+                <img src="<?Php echo base_url()?>/assets/images/Prog.jpg" class="img-responsive img-circle">
+            </div>
+            <div class="col-lg-8">
+                Emmanuel Kgatla
+            </div>
+        </div>
+    </div>
 </div>
