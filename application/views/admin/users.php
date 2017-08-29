2b16bf5fa0e73305752d306d0e59a847db655f7d
Emmanuel <emmkga@gmail.com>
2017-08-29 10:25:21 +0200

    User page update - Mannu

--- application/views/admin/users.php
+++ application/views/admin/users.php
@@ -1,15 +1,46 @@
 <div class="row">
-    <div class="col-lg-8">
-        <button type="submit" data-toggle="modal" data-target="#add_user_modal">Add user</button>
-        <button type="submit">Export PDF</button>
-        <button type="submit">Export Excel</button>
-        <button type="submit">Print</button>
-        <button type="submit">Email</button>
-    </div>
-    <div class="col-lg-4">
-        <form>
-            <input type="text" id="search" name="search" class="">
-        </form>
+    <div class="controls">
+        <div class="col-lg-8">
+            <div class="btn-controls-group">
+                <button class="btn-controls" id="add-user" type="submit" data-toggle="modal" data-target="#add_user_modal">
+                    <i class="fa fa-plus"></i> Add user
+                </button>
+                <!--<button class="btn-controls" id="export-pdf" type="submit">Export PDF</button>
+                <button class="btn-controls" id="" type="submit">Export Excel</button>-->
+                <button class="btn-controls" id="email" type="submit"><i class="fa fa-envelope"></i> Email</button>
+                <button class="btn-controls" id="print" type="submit"><i class="fa fa-print"></i> Print</button>
+                <div id="export-control" class="dropdown">
+                    <button class="btn btn-default dropdown-toggle" type="button" id="export-option" 
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <i class="fa fa-exchange"></i> Export
+                        <span class="caret"></span>
+                    </button>
+                    <ul id="dropdown-menu-fix" class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <li><a href="#">PDF</a></li>
+                        <li><a href="#">Word</a></li>
+                        <li><a href="#">Excel</a></li>
+                        <!--<li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>-->
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <label id="control-show-lbl">
+                Show 
+                <input type="" name="" id="control-show">
+            </label>
+        </div>
+        <div class="col-lg-2">
+            <div class="search-control">
+                <form id="frm-search" class="" action="" method="">
+                    <input type="search" id="search" name="search" class="search">
+                    <span id = "mg-glass" class="glyphicon glyphicon-search"></span>
+                </form>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
     </div>
     <div class="col-lg-12">
         <form method="post" id="form_add">
@@ -84,62 +115,71 @@
 </div>
 <div class="row">
     <div class="col-lg-12">
-        <!--<table class="table table-striped table-bordered">
+        <table class="table table-bordered">
             <thead>
                 <th>No.</th>
+                <th>Picture</th>
                 <th>Surname</th>
                 <th>First name</th>
                 <th>Contact number</th>
                 <th>Email address</th>
                 <th>User role</th>
-                <th colspan="3">Action</th>
+                <th colspan="4">Action</th>
             </thead>
             <tbody>
                 <tr>
                     <td>1</td>
+                    <td><img src="<?Php echo base_url()?>assets/images/pic06.jpg" class="img-circle" height="35" width="35"></td>
                     <td>Kgatla</td>
                     <td>Emmanuel</td>
                     <td>062 023 6010</td>
                     <td>emmanuel66@live.co.za</td>
                     <td>Manager</td>
-                    <td>Permissions</td>
-                    <td>Edit</td>
-                    <td>Delete</td>
+                    <td><a href="#"><i class="fa fa-pencil"></i></a></td>
+                    <td><a href="#"><i class="fa fa-eye"></i></a></td>
+                    <td><a href="#"><i class="fa fa-lock"></i></a></td>
+                    <td><a href="#"><i class="fa fa-trash"></i></a></td>
                 </tr>
                 <tr>
                     <td>2</td>
+                    <td><img src="<?Php echo base_url()?>assets/images/pic06.jpg" class="img-circle" height="35" width="35"></td>
                     <td>Sibiya</td>
                     <td>Joseph</td>
                     <td>062 023 6010</td>
                     <td>jose@live.co.za</td>
                     <td>Receptionist</td>
-                    <td>Permissions</td>
-                    <td>Edit</td>
-                    <td>Delete</td>
+                    <td><a href="#"><i class="fa fa-pencil"></i></a></td>
+                    <td><a href="#"><i class="fa fa-eye"></i></a></td>
+                    <td><a href="#"><i class="fa fa-lock"></i></a></td>
+                    <td><a href="#"><i class="fa fa-trash"></i></a></td>
                 </tr>
                 <tr>
                     <td>3</td>
+                    <td><img src="<?Php echo base_url()?>assets/images/pic06.jpg" class="img-circle" height="35" width="35"></td>
                     <td>Madella</td>
                     <td>Lee-Roy</td>
                     <td>062 023 6010</td>
                     <td>roy@live.co.za</td>
                     <td>Staff</td>
-                    <td>Permissions</td>
-                    <td>Edit</td>
-                    <td>Delete</td>
+                    <td><a href="#"><i class="fa fa-pencil"></i></a></td>
+                    <td><a href="#"><i class="fa fa-eye"></i></a></td>
+                    <td><a href="#"><i class="fa fa-lock"></i></a></td>
+                    <td><a href="#"><i class="fa fa-trash"></i></a></td>
                 </tr>
                 <tr>
                     <td>4</td>
+                    <td><img src="<?Php echo base_url()?>assets/images/pic06.jpg" class="img-circle" height="35" width="35"></td>
                     <td>Mojela</td>
                     <td>Tshego</td>
                     <td>062 023 6010</td>
                     <td>tshego@live.co.za</td>
                     <td>Practitioner</td>
-                    <td>Permissions</td>
-                    <td>Edit</td>
-                    <td>Delete</td>
+                    <td><a href="#"><i class="fa fa-pencil"></i></a></td>
+                    <td><a href="#"><i class="fa fa-eye"></i></a></td>
+                    <td><a href="#"><i class="fa fa-lock"></i></a></td>
+                    <td><a href="#"><i class="fa fa-trash"></i></a></td>
                 </tr>
             </tbody>
-        </table>-->
+        </table>
     </div>
 </div>
