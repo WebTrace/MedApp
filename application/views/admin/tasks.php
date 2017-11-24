049edd294f12cbf84892e152965781b6534b44f4
Joseph <sibiyajvt@gmail.com>
2017-11-24 08:59:40 +0200

    Task Page
    Task Page

--- application/views/admin/tasks.php
+++ application/views/admin/tasks.php
@@ -99,3 +99,50 @@
         </div>
     </div>
 </div>
+<div id="exTab2" class="container">	  
+    <ul class="nav nav-tabs">  
+      <li class="active">  
+          <a  href="#1" data-toggle="tab"><span class="fa fa-eye"></span>  View Task Details</a>  
+       </li>  
+       <li><a href="#2" data-toggle="tab"><span class="fa fa-plus"></span>  New Task</a>  
+      </li>  
+      <li><a href="#3" data-toggle="tab"><span class="fa fa-edit"></span>  Edit Task</a>  
+       </li>  
+       <li><a href="#4" data-toggle="tab"><span class="fa fa-print"></span>  Print Task</a>  
+       </li>  
+      <li><a href="#5" data-toggle="tab"><span class="fa fa-trash-o"></span>  Delete Task</a>  
+       </li>  
+    </ul>  
+    <table class="table table-bordered table-style table-responsive" style="height: 25px">  
+          <tr>  
+            <th colspan="2" style="border-right: none"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span></a></th>  
+            <th colspan="3"> November - 2017</th>  
+            <th colspan="2" style="border-left: none"><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></th>  
+          </tr>  
+    </table>  
+   <div class="table-data">  
+     <table class="table table-hover" cellpadding="0" cellspacing="0" border="0">  
+       <thead>  
+          <tr>  
+            <th>Task No.</th>  
+            <th>Priority</th>  
+            <th>Task Name</th>  
+            <th colspan="3">Description</th>  
+           <th colspan="2">Schedule Start</th>  
+            <th colspan="2">Schedule End</th>  
+            <th>Estimated Effort</th>  
+          </tr>  
+        </thead>  
+        <tbody>  
+          <tr>  
+            <td>1</td>  
+           <td>Medium</td>  
+            <td>Add Patient</td>  
+            <td colspan="3">kgdfkflhgdjkfghdfghfdgkdldfh</td>  
+            <td colspan="2">20/11/17 2:30 pm</td>  
+            <td colspan="2">20/11/17 2:30 pm</td>  
+            <td>2hrs</td>  
+          </tr>  
+        </tbody>  
+   </table>  
+</div>  
